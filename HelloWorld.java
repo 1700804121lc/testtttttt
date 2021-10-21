860c18a535751c670940a1f9b48d35c152830763
lc <1067163741@qq.com>
2021-10-21 14:17:38 +0800

    第一次提交

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+
+/**
+ * 
+ * @author lc
+ * @version 创建时间：2021年10月21日 下午2:16:22
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaa");
+    }
+}
